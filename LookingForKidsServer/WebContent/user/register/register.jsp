ef4cfe48123d9eaf69274095ee720a7d60f3fcbe
dalecgu <131250169@smail.nju.edu.cn>
2015-07-09 21:27:25 +0800

    增加了register相关的（不完整）

---
+++ LookingForKidsServer/WebContent/user/register/register.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Register</title>
+</head>
+<body>
+	<s:form action="register">
+		<s:textfield name="userRegisterForm.nickName" label="昵称" />
+		<s:textfield name="userRegisterForm.email" label="电子邮箱" />
+		<s:password name="userForm.password" label="密码"></s:password>
+		<s:textfield name="userRegisterForm.phonenumber" label="手机号码" />
+		<s:submit value="注册"></s:submit>
+	</s:form>
+</body>
+</html>
---
+++ LookingForKidsServer/src/form/UserRegisterForm.java
@@ -0,0 +1,41 @@
+package form;
+
+public class UserRegisterForm {
+
+	private String nickName;
+	private String email;
+	private String password;
+	private String phonenumber;
+	
+	public String getNickName() {
+		return nickName;
+	}
+	
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+}
