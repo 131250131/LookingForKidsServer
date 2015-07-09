5181fb8cdbf41008b74bf14f8c074d2a383a5d45
dalecgu <131250169@smail.nju.edu.cn>
2015-07-09 15:03:30 +0800

    数据库建立调整

--- LookingForKidsServer/src/common/DataBaseCreator.java
+++ LookingForKidsServer/src/common/DataBaseCreator.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class DataBaseCreator {
-	String url="jdbc:mysql://localhost/u_csc";
+	String url="jdbc:mysql://localhost";
 	String user="root";
 	String password="hongmao";
 	String sql="";
