3eb5f2c66ad25bba1b186c150a4c21946373707c
qifen <547799503@qq.com>
2015-07-11 15:16:55 +0800

    123

--- LookingForKidsServer/src/action/PublishAction.java
+++ LookingForKidsServer/src/action/PublishAction.java
@@ -1,7 +1,13 @@
 package action;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
+
+import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -60,7 +66,24 @@ public class PublishAction extends ActionSupport{
 
 	public String execute(){
 		try {
-//			String root = ServeletActionContext.
+			@SuppressWarnings("deprecation")
+			String root = ServletActionContext.getRequest().getRealPath("/upload");
+			for(int i=0;i<file.size();i++){
+				 InputStream is = new FileInputStream(file.get(i));
+		            
+		         //得到图片保存的位置(根据root来得到图片保存的路径在tomcat下的该工程里)
+		         File destFile = new File(root,this.getFileFileName().get(i));
+		            
+		         //把图片写入到上面设置的路径里
+		         OutputStream os = new FileOutputStream(destFile);
+		         byte[] buffer = new byte[400];
+		         int length  = 0 ;
+		         while((length = is.read(buffer))>0){
+		             os.write(buffer, 0, length);
+		         }
+		         is.close();
+		         os.close();	
+			}
 			userManager.publish(kidPublishForm);
 			return SUCCESS;
 		} catch (Exception e) {
