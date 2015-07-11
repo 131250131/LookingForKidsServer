609519d5672cb8845d6dce55e358e1305ab6e803
131250131 <275838287@qq.com>
2015-07-11 18:53:36 +0800

    测试图片更新。

--- LookingForKidsServer/src/face/Main.java
+++ LookingForKidsServer/src/face/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		FaceRecognization faceRecognization = new FaceRecognization();
 		faceRecognization.train("./res/recognizer/train/");
+		System.out.println("Training completed.");
 		faceRecognization.recognize("./res/recognizer/test/test.jpg", "./res/recognizer/trained.xml");
 	}
 
--- LookingForKidsServer/src/face/PhotoMappingTable.java
+++ LookingForKidsServer/src/face/PhotoMappingTable.java
@@ -6,8 +6,10 @@ public class PhotoMappingTable extends MappingTable<Integer, String> {
 
 	public PhotoMappingTable() {
 		super();
-		this.map.put(1, "·¶±ù±ù");
-		this.map.put(2, "ÁøÑÒ");
+		this.map.put(1, "³ÂÊ¥³¬");
+		this.map.put(2, "ĞìÏöÓî");
+		this.map.put(3, "ÂŞ»áÏé");
+		this.map.put(4, "ÕÔÕıÇä");
 	}
 	
 }
